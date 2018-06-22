bd0c40f92259b6aacabb4186055e6bb134badcd6
Thomas Sjolshagen <eighty20results@users.noreply.github.com>
2018-06-22 08:19:11 -0400

    BUG FIX: Cannot call function in `empty()`

--- includes/functions.php
+++ includes/functions.php
@@ -20,9 +20,11 @@ function pmprowoo_get_membership_products_from_order( $order_id ) {
 	
 	// Get the order object.
 	$order = new WC_Order($order_id);
+	$order_user_id = is_object( $order ) ? $order->get_user_id() : null;
+	$order_items = is_object( $order ) ? $order->get_items() : array();
 	
 	// If there are no membership products or items in the order, return an empty array.
-	if( empty( $pmprowoo_product_levels ) || ( empty( $order->get_user_id() ) && sizeof( $order->get_items() ) > 0) ) {
+	if( empty( $pmprowoo_product_levels ) || ( empty( $order_user_id ) && !empty( $order_items ) ) ) {
 		return $membership_products;
 	}
 
@@ -30,7 +32,7 @@ function pmprowoo_get_membership_products_from_order( $order_id ) {
 	$membership_product_ids = array_keys($pmprowoo_product_levels);
 	
 	// Are there any membership products?
-	foreach( $order->get_items() as $item ) {
+	foreach( $order_items as $item ) {
 		if( $item['product_id'] > 0 && in_array( $item['product_id'], $membership_product_ids) ) 	//not sure when a product has id 0, but the Woo code checks this
 			$membership_products[] = $item['product_id'];
 	}
