da45a6dee33736f45b665930d10a4aa45b8c7b38
Andrew Lima <andrew@arctek.co.za>
2023-01-24 19:35:25 +0200

    Escaping (Needs some testing)
    Escaping

--- includes/admin.php
+++ includes/admin.php
@@ -10,8 +10,8 @@
 function pmprowoo_plugin_row_meta( $links, $file ) {
 	if ( strpos( $file, 'pmpro-woocommerce.php' ) !== false ) {
 		$new_links = array(
-			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/add-ons/pmpro-woocommerce/' ) . '" title="' . esc_attr( __( 'View Documentation', 'pmpro-woocommerce' ) ) . '">' . __( 'Docs', 'pmpro-woocommerce' ) . '</a>',
-			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/support/' ) . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro-woocommerce' ) ) . '">' . __( 'Support', 'pmpro-woocommerce' ) . '</a>',
+			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/add-ons/pmpro-woocommerce/' ) . '" title="' . esc_attr__( 'View Documentation', 'pmpro-woocommerce' ) . '">' . esc_html__( 'Docs', 'pmpro-woocommerce' ) . '</a>',
+			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/support/' ) . '" title="' . esc_attr__( 'Visit Customer Support Forum', 'pmpro-woocommerce' ) . '">' . esc_html__( 'Support', 'pmpro-woocommerce' ) . '</a>',
 		);
 		$links     = array_merge( $links, $new_links );
 	}
